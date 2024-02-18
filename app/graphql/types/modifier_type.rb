b4556dba7b6ccfab6f24d34c409b3253c5dd6228
fauzannadhif <fauzannadhifm98@gmail.com>
2024-02-18 17:02:13 +0800

    update graphql modifier type

--- app/graphql/types/modifier_type.rb
+++ app/graphql/types/modifier_type.rb
@@ -8,6 +8,7 @@ module Types
     field :display_order, Integer
     field :default_quantity, Integer
     field :price_override, Float
+    field :item, Types::ItemType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
