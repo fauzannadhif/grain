266022e8546badc7bda8f76174c30e990e968e8f
fauzannadhif <fauzannadhifm98@gmail.com>
2024-02-18 13:20:28 +0800

    initial db migrate and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,57 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+MENUS = [
+  {
+    identifier: 'M0',
+    label: 'Pizza',
+    state: 'Active',
+    start_date: DateTime.new(2024, 1, 1),
+    end_date: DateTime.new(2025, 1, 1)
+  }
+]
+
+MENUS.each do |menu|
+  record = Menu.create!(
+    identifier: menu[:identifier],
+    label: menu[:label],
+    state: menu[:state],
+    start_date: menu[:start_date],
+    end_date: menu[:end_date]
+  )
+  puts "Menu #{record[:identifier]} is successfully inserted"
+end
+
+SECTIONS = [
+  {
+    identifier: 'S0',
+    label: 'Classic Pizzas',
+    description: 'classic flavored pizzas'
+  }
+]
+
+SECTIONS.each do |section|
+  record = Section.create!(
+    identifier: section[:identifier],
+    label: section[:label],
+    description: section[:state],
+  )
+  puts "Section #{record[:identifier]} is successfully inserted"
+end
+
+MENU_SECTION = [
+  {
+    menu: Menu.find_by(identifier: 'M0'),
+    section: Section.find_by(identifier: 'S0'),
+    display_order: 0
+  }
+]
+
+MENU_SECTION.each do |menu_section|
+  record = MenuSection.create!(
+    menu: menu_section[:menu],
+    section: menu_section[:section],
+    display_order: menu_section[:display_order],
+  )
+end
